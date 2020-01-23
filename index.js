83a1966e23331dd212c07be8c4792f6d8b76bdf8
triszt4n <trisztanpiller@gmail.com>
2020-01-23 13:46:30 +0100

    working on static

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('static'));
+app.listen(port, function () {
+    console.log(`Listening on: http://localhost:${port}/`);
+});
